e78af3eefc991aa1c37f0b6a3367825bde28b04b
Christoffer Aakre <christoffer.corfield@gmail.com>
2021-12-23 22:42:36 +0100

    Use ffmpeg instead of mpv to take screenshots
    Added screenshot error enum
    
    Use ffmpeg for screenshots and return error
    
    Use ffmpeg for screenhots, fall back to mpv if ffmpeg fails

--- utils/ankiexport.py
+++ utils/ankiexport.py
@@ -4,14 +4,20 @@ import time
 import json
 import os
 
+from enum import Enum
+
+class Errors(Enum):
+    FFMPEG_SCREENSHOT_ERROR = 1
+    MPV_SCREENSHOT_ERROR = 2
+    SCREENSHOT_ERROR = 3
 
 class AnkiExporter():
 
     def __init__(self):
-        
+
         self.mpv_executable = 'mpv'
         self.mpv_cwd = os.path.expanduser('~')
-        
+
         self.tmp_dir = '.'
 
         self.migaku_dict_host = '127.0.0.1'
@@ -41,9 +47,8 @@ class AnkiExporter():
         audio_path = os.path.normpath(audio_path)
 
         audio_proc = self.make_audio(media_file, audio_track, time_start, time_end, audio_path)
-        screenshot_proc = self.make_snapshot(media_file, time_start, time_end, img_path)
+        error, screenshot_proc = self.make_snapshot(media_file, time_start, time_end, img_path)
         audio_proc.wait()
-        screenshot_proc.wait()
 
         try:
             img_file = open(img_path,'rb')
@@ -80,7 +85,7 @@ class AnkiExporter():
             return -2
 
         return 0
-        
+
 
 
     def make_audio(self, media_file, audio_track, start, end, out_path):
@@ -93,7 +98,42 @@ class AnkiExporter():
         return subprocess.Popen(args, cwd=self.mpv_cwd)
 
 
-    def make_snapshot(self, media_file, start, end, out_path):
+    def ffmpeg_screenshot(self, media_file, start, end, out_path):
+        args = [
+                'ffmpeg',
+                '-y', '-loglevel', 'error',
+                '-ss', str((start + end) / 2),
+                '-i', media_file,
+                '-vframes', '1',
+                out_path
+                ]
+
+        # See https://ffmpeg.org/ffmpeg-filters.html#scale-1 for scaling options
+
+        # None or values smaller than 1 set the axis to auto
+        w = self.image_width
+        if w is None or w < 1:
+            w = -1
+        h = self.image_height
+        if h is None or h < 1:
+            h = -1
+
+        # Only apply filter if any axis is set to non-auto
+        if w > 0 or h > 0:
+                args[-1:-1] = [
+                    '-filter:v',
+                    'scale=w=\'min(iw,%d)\':h=\'min(ih,%d)\':force_original_aspect_ratio=decrease'
+                    % (w, h)
+                ]
+        error = None
+        proc = subprocess.Popen(args, cwd=self.mpv_cwd)
+        proc.wait()
+        # Check that image was saved
+        if not os.path.exists(out_path):
+            error = Errors.FFMPEG_SCREENSHOT_ERROR
+        return error, proc
+
+    def mpv_screenshot(self, media_file, start, end, out_path):
         args = [self.mpv_executable, '--load-scripts=no',                                       # start mpv without scripts
                 media_file, '--loop-file=no', '--audio=no', '--no-ocopy-metadata', '--no-sub',  # just play video
                 '--frames=1',                                                                   # for one frame
@@ -116,4 +156,25 @@ class AnkiExporter():
             scale_arg = '--vf-add=scale=w=%d:h=%d:force_original_aspect_ratio=decrease' % (w, h)
             args.append(scale_arg)
 
-        return subprocess.Popen(args, cwd=self.mpv_cwd)
+        error = None
+        proc = subprocess.Popen(args, cwd=self.mpv_cwd)
+        proc.wait()
+        # Check that image was saved
+        if not os.path.exists(out_path):
+            error = Errors.MPV_SCREENSHOT_ERROR
+
+        return error, proc
+
+    def make_snapshot(self, media_file, start, end, out_path):
+        # Default to using ffmpeg for screenshots
+        error, proc = self.ffmpeg_screenshot(media_file, start, end, out_path)
+        # Fall back to mpv if ffmpeg fails
+        if error == Errors.FFMPEG_SCREENSHOT_ERROR:
+            error, proc = self.mpv_screenshot(media_file, start, end, out_path)
+
+        # If error is MPV_SCREENSHOT_ERROR, then both ffmpeg
+        # and mpv failed, so we return a more generic error
+        # if error == Errors.MPV_SCREENSHOT_ERROR:
+            # error = Errors.SCREENSHOT_ERROR
+
+        return error, proc
